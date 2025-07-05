b886f873ef4dc1fc41e1d90f67c2dc10d76e9596
ventsislavVidulov <xcen3k@gmail.com>
2025-07-05 15:02:27 +0300

    create rental order class

---
+++ SirmaOOPExam/Core/Models/RentalOrder.cs
@@ -0,0 +1,30 @@
+﻿using SirmaOOPExam.Core.Interfaces;
+
+namespace SirmaOOPExam.Core.Models
+{
+    internal class RentalOrder : IRentalOrder
+    {
+        public int Id { get; }
+
+        public Car Car { get; private set; }
+        // Car is muttable so a "reserved" order can change the car
+        public Customer Customer { get; }
+
+        public DateOnly StartDate { get; }
+
+        public int PeriodInDays { get; private set; }
+        // Period can be longened if the customer wants to keep the car longer
+
+        public DateOnly ExpectedReturnDate { get; }
+
+        public RentalOrder(int id, Car car, Customer customer, DateOnly startDate, int periodInDays)
+        {
+            Id = id;
+            Car = car;
+            Customer = customer;
+            StartDate = startDate;
+            PeriodInDays = periodInDays;
+            ExpectedReturnDate = startDate.AddDays(periodInDays);
+        }
+    }
+}
