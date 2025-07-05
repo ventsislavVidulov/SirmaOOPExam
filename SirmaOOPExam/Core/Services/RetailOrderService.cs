27a52a88b5a448aaa118d6667518d4663d097610
ventsislavVidulov <xcen3k@gmail.com>
2025-07-05 17:14:26 +0300

    RentalOrderService - initialization

---
+++ SirmaOOPExam/Core/Services/RetailOrderService.cs
@@ -0,0 +1,29 @@
+﻿using SirmaOOPExam.Core.Interfaces;
+
+namespace SirmaOOPExam.Core.Services
+{
+    internal class RetailOrderService
+    {
+        private List<IRentalOrder> _rentalOrders;
+
+        public RetailOrderService(List<IRentalOrder> rentalOrders)
+        {
+            _rentalOrders = rentalOrders;
+        }
+
+        public void AddRentalOrder(IRentalOrder rentalOrder)
+        {
+            _rentalOrders.Add(rentalOrder);
+        }
+
+        public IEnumerable<IRentalOrder> GetAllRentalOrders()
+        {
+            return _rentalOrders.AsReadOnly();
+        }
+
+        public IRentalOrder? GetRentalOrderById(int id)
+        {
+            return _rentalOrders.FirstOrDefault(ro => ro.Id == id);
+        }
+    }
+}
