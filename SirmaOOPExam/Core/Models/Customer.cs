b17590d9fd8603d3c01954cd809526fc0e0f1917
ventsislavVidulov <xcen3k@gmail.com>
2025-07-05 15:32:13 +0300

    Customer - add Id, Name and constructor

--- SirmaOOPExam/Core/Models/Customer.cs
+++ SirmaOOPExam/Core/Models/Customer.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SirmaOOPExam.Core.Interfaces;
 
 namespace SirmaOOPExam.Core.Models
 {
-    internal class Customer
+    internal class Customer : ICustomer
     {
+        public int Id { get; }
+        public string Name { get; }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
