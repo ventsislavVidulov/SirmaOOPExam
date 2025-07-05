d02cb9a52c1ffbc8d3e1a85926c6341b838e596b
ventsislavVidulov <xcen3k@gmail.com>
2025-07-05 12:35:55 +0300

    Create car interface

---
+++ SirmaOOPExam/Core/Interfaces/ICar.cs
@@ -0,0 +1,14 @@
+﻿using SirmaOOPExam.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirmaOOPExam.Core.Interfaces
+{
+    internal interface ICar
+    { 
+        public void UpdateCar(int id, string make, string model, int year, string type, string availability, string customer);
+    }
+}
