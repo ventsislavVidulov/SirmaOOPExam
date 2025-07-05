daa367a6edb929a46bad4cc35d92d401c4ae1a1d
ventsislavVidulov <xcen3k@gmail.com>
2025-07-05 14:04:21 +0300

    Create rental order interface

---
+++ SirmaOOPExam/Core/Interfaces/IRentalOrder.cs
@@ -0,0 +1,18 @@
+﻿using SirmaOOPExam.Core.Models;
+
+namespace SirmaOOPExam.Core.Interfaces
+{
+    internal interface IRentOrder
+    {
+        int Id { get; }
+        Car Car { get; }
+
+        Customer Customer { get; }
+
+        DateTime RentalStartDate { get; }
+
+        int RentalDays { get; }
+
+        DateTime ExpectedReturnDate { get; }
+    }
+}
