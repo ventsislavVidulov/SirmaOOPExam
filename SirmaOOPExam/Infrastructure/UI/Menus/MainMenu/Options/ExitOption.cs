afa9d25ee7ed1b5b8db41637b012591fa913d635
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 21:33:59 +0300

    ExitOption - init

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/MainMenu/Options/ExitOption.cs
@@ -0,0 +1,16 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.MainMenu.Options
+{
+    internal class ExitOption : IMenuOption
+    {
+        public string MenuText { get; } = "Exit";
+        public int CommandNumber { get; } = 4;
+
+        public void Execute()
+        {
+            Console.WriteLine("Exiting the application. Goodbye!");
+            Environment.Exit(0);
+        }
+    }
+}
