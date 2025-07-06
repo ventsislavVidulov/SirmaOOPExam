83c4910e23f83f275e0ba610845cd6aa314b71c7
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 21:33:32 +0300

    IRentalOrderService - add ReturnCar method

--- SirmaOOPExam/Core/Interfaces/IRentalOrderService.cs
+++ SirmaOOPExam/Core/Interfaces/IRentalOrderService.cs
@@ -3,6 +3,7 @@
     internal interface IRentalOrderService
     {
         void AddRentalOrder(IRentalOrder rentalOrder);
+        void ReturnCar(int carId);
         List<IRentalOrder> GetAllRentalOrders();
         List<ICar> GetCars();
         List<ICustomer> GetCustomers();
