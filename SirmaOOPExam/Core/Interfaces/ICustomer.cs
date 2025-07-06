2698b401708212dbbcbac37742cccc076ca9ace8
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 19:49:56 +0300

    ICustomer - make the access modifier to public

--- SirmaOOPExam/Core/Interfaces/ICustomer.cs
+++ SirmaOOPExam/Core/Interfaces/ICustomer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SirmaOOPExam.Core.Interfaces
 {
-    internal interface ICustomer
+    public interface ICustomer
     {
         public int Id { get; }
         public string Name { get; }
