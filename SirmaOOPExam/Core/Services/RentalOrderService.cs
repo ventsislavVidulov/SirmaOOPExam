c997dab3b5d9d509256f8d0e081774db626fefee
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 18:47:51 +0300

    RentalOrderService - sdd methods to get cars and customers

--- SirmaOOPExam/Core/Services/RentalOrderService.cs
+++ SirmaOOPExam/Core/Services/RentalOrderService.cs
@@ -2,23 +2,36 @@
 
 namespace SirmaOOPExam.Core.Services
 {
-    internal class RentalOrderService
+    internal class RentalOrderService : IRentalOrderService
     {
         private List<IRentalOrder> _rentalOrders;
+        private List<ICar> _cars;
+        private List<ICustomer> _customers;
 
-        public RentalOrderService(List<IRentalOrder> rentalOrders)
+        public RentalOrderService(List<IRentalOrder> rentalOrders, List<ICar> cars, List<ICustomer> customers)
         {
             _rentalOrders = rentalOrders;
+            _cars = cars;
+            _customers = customers;
         }
 
+        public List<ICar> GetCars()
+        {
+            return _cars;
+        }
+
+        public List<ICustomer> GetCustomers()
+        {
+            return _customers;
+        }
         public void AddRentalOrder(IRentalOrder rentalOrder)
         {
             _rentalOrders.Add(rentalOrder);
         }
 
-        public IEnumerable<IRentalOrder> GetAllRentalOrders()
+        public List<IRentalOrder> GetAllRentalOrders()
         {
-            return _rentalOrders.AsReadOnly();
+            return _rentalOrders;
         }
 
         public IRentalOrder? GetRentalOrderById(int id)
