203388243c90e880531db2ed7d9883c081df818b
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 11:45:15 +0300

    IFileManagerAsync - initialization

---
+++ SirmaOOPExam/Infrastructure/FileManagment/Interfaces/IFileManagerAsync.cs
@@ -0,0 +1,9 @@
+﻿using SirmaOOPExam.Core.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.FileManagment.Interfaces
+{
+    internal interface IFileManagerAsync
+    {
+        Task<List<ICar>> GetCarsFromFile();
+    }
+}
