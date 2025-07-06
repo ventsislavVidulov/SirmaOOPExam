0afa2ceea96e88ffbfe2dcc26368de4f5dda517a
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 11:36:39 +0300

    ICSVFilemanipulator - initialization

---
+++ SirmaOOPExam/Infrastructure/FileManagment/Interfaces/ICSVFileManipulator.cs
@@ -0,0 +1,8 @@
+﻿namespace SirmaOOPExam.Infrastructure.FileManagment.Interfaces
+{
+    internal interface ICSVFileManipulator
+    {
+        Task<List<string[]>> ReadCsvAsync(string filePath);
+        Task WriteCsvAsync(string filePath, List<string[]> data);
+    }
+}
