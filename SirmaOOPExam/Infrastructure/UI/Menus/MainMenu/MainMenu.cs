c27beee1c9521f9e2bff65c31864f4d6298239f5
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 19:03:27 +0300

    MainMenu - init

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/MainMenu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+using SirmaOOPExam.Infrastructure.UserInterface;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.MainMenu
+{
+    internal class MainMenu : IMenu
+    {
+        public string Title { get; set; } = "Main Menu";
+
+        public List<IMenuOption> Options = new();
+
+        public void RegisterOption(IMenuOption option)
+        {
+            Options.Add(option);
+        }
+        public int DisplayMenu()
+        {
+            ConsoleView.DisplayHeader(Title);
+            foreach (var menuItem in Options)
+            {
+                ConsoleView.DisplayOption(menuItem);
+            }
+            int choice = ConsoleView.GetIntegerInput($"Your choice(1-{Options.Count}): ");
+            return choice;
+        }
+    }
+}
