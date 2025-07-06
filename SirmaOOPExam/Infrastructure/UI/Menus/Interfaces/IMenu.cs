a7bd62c13918668f0463d65cb91658a90157e294
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 19:50:55 +0300

    IMenu - add RegisterOption() method

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/Interfaces/IMenu.cs
@@ -0,0 +1,9 @@
+﻿namespace SirmaOOPExam.Infrastructure.UI.Menus.Interfaces
+{
+    internal interface IMenu
+    {
+        string Title { get; set; }
+        int DisplayMenu();
+        void RegisterOption(IMenuOption option);
+    }
+}
