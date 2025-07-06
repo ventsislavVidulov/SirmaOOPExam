2c68b4e741c69abcb7d9ee064e195c404bc130ce
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 12:40:26 +0300

    FileManagerAsync - implement all methods from IFileManagerAsync

--- SirmaOOPExam/Infrastructure/FileManagment/FileManagerAsync.cs
+++ SirmaOOPExam/Infrastructure/FileManagment/FileManagerAsync.cs
@@ -1,11 +1,13 @@
-﻿using SirmaOOPExam.Core.Interfaces;
-using SirmaOOPExam.Core.Models;
+﻿using SirmaOOPExam.Core.Models;
 using SirmaOOPExam.Infrastructure.FileManagment.Interfaces;
 
 namespace SirmaOOPExam.Infrastructure.FileManagment
 {
-    internal class FileManagerAsync 
-        //: IFileManagerAsync
+    //this class orchestrates the conversion of class members to string arrays and vice versa
+    //and writes and reads the data from the CSV files asynchronously
+    //the methods generic parameter resive concreate classes because the Converter uses
+    //reflection to convert the objects
+    internal class FileManagerAsync : IFileManagerAsync
     {
         private string _filePathCar = "../../../Infrastructure/FileManagment/Data/cars.csv";
         private string _filePathCustomer = "../../../Infrastructure/FileManagment/Data/customers.csv";
@@ -19,15 +21,98 @@ namespace SirmaOOPExam.Infrastructure.FileManagment
             _converter = converter;
         }
 
-        public async Task<List<Car>> GetCarsFromFile()
+        //gets all cars from the file
+        public async Task<List<Car>> GetAllCarsFromFile()
         {
             List<Car> cars = new List<Car>();
-            var rawData = await _cSVFileManipulator.ReadCsvAsync(_filePathCar);
-            foreach (var item in rawData)
+            //reads the CSV file and convert each line to a Car object
+            List<string[]> rawData = await _cSVFileManipulator.ReadCsvAsync(_filePathCar);
+            if (rawData == null || rawData.Count == 0)
             {
-                cars.Add(_converter.ConvertToObject<Car>(item));
+                return cars;
             }
-            return cars;
+            else
+            {
+                foreach (var item in rawData)
+                {
+                    //convert each string array to a Car object using the converter
+                    cars.Add(_converter.ConvertToObject<Car>(item));
+                }
+                return cars;
+            }
+        }
+
+        //saves all cars to the file
+        public async Task SaveAllCarsToFile(List<Car> cars)
+        {
+            List<string[]> rawData = new List<string[]>();
+            //converts each Car object to a string array and add it to the rawData list
+            foreach (var car in cars)
+            {
+                rawData.Add(_converter.ConvertToStringArray(car));
+            }
+            // writes the rawData list to the CSV file
+            await _cSVFileManipulator.WriteCsvAsync(_filePathCar, rawData);
+        }
+
+        //gets all customers from the file, the logic is similar to GetAllCarsFromFile
+        public async Task<List<Customer>> GetAllCustomersFromFile()
+        {
+            List<Customer> customers = new List<Customer>();
+            var rawData = await _cSVFileManipulator.ReadCsvAsync(_filePathCustomer);
+            if (rawData == null || rawData.Count == 0)
+            {
+                return customers;
+            }
+            else
+            {
+                foreach (var item in rawData)
+                {
+                    customers.Add(_converter.ConvertToObject<Customer>(item));
+                }
+                return customers;
+            }
+        }
+
+        //saves all customers to the file, the logic is similar to SaveAllCarsToFile
+        public async Task SaveAllCustomersToFile(List<Customer> customers)
+        {
+            List<string[]> rawData = new List<string[]>();
+            foreach (var customer in customers)
+            {
+                rawData.Add(_converter.ConvertToStringArray(customer));
+            }
+            await _cSVFileManipulator.WriteCsvAsync(_filePathCustomer, rawData);
+        }
+
+        //gets all rental orders from the file, the logic is similar to GetAllCarsFromFile
+        public async Task<List<RentalOrder>> GetAllRentalOrdersFromFile()
+        {
+            List<RentalOrder> rentalOrders = new List<RentalOrder>();
+            var rawData = await _cSVFileManipulator.ReadCsvAsync(_filePathRentalOrder);
+            if (rawData == null || rawData.Count == 0)
+            {
+                return rentalOrders;
+            }
+            else
+            {
+                foreach (var item in rawData)
+                {
+                    rentalOrders.Add(_converter.ConvertToObject<RentalOrder>(item));
+                }
+                return rentalOrders;
+            }
+        }
+
+        //saves all rental orders to the file, the logic is similar to SaveAllCarsToFile
+        public async Task SaveAllRentalOrdersToFile(List<RentalOrder> rentalOrders)
+        {
+            List<string[]> rawData = new List<string[]>();
+            foreach (var order in rentalOrders)
+            {
+                rawData.Add(_converter.ConvertToStringArray(order));
+            }
+            await _cSVFileManipulator.WriteCsvAsync(_filePathRentalOrder, rawData);
         }
     }
 }
