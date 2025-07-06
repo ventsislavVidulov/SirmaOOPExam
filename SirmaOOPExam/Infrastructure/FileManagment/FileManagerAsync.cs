bb916afff6762be8ed1873c948f43c9fa2e35ed4
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 11:50:00 +0300

    FileManagerAsync - initialize the fila manager that orchestrates the Converter and CSVFileManipulator

---
+++ SirmaOOPExam/Infrastructure/FileManagment/FileManagerAsync.cs
@@ -0,0 +1,33 @@
+﻿using SirmaOOPExam.Core.Interfaces;
+using SirmaOOPExam.Core.Models;
+using SirmaOOPExam.Infrastructure.FileManagment.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.FileManagment
+{
+    internal class FileManagerAsync 
+        //: IFileManagerAsync
+    {
+        private string _filePathCar = "../../../Infrastructure/FileManagment/Data/cars.csv";
+        private string _filePathCustomer = "../../../Infrastructure/FileManagment/Data/customers.csv";
+        private string _filePathRentalOrder = "../../../Infrastructure/FileManagment/Data/rental_orders.csv";
+
+        private ICSVFileManipulator _cSVFileManipulator;
+        private IConverter _converter;
+        public FileManagerAsync(ICSVFileManipulator cSVFileManipulator, IConverter converter)
+        {
+            _cSVFileManipulator = cSVFileManipulator;
+            _converter = converter;
+        }
+
+        public async Task<List<Car>> GetCarsFromFile()
+        {
+            List<Car> cars = new List<Car>();
+            var rawData = await _cSVFileManipulator.ReadCsvAsync(_filePathCar);
+            foreach (var item in rawData)
+            {
+                cars.Add(_converter.ConvertToObject<Car>(item));
+            }
+            return cars;
+        }
+    }
+}
