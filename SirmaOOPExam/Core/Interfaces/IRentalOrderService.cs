2a1bd86011f08042ddf10beae6e25a850ff0f32d
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 18:46:09 +0300

    IRentalOrderService - change the types of IEnumerable to List

---
+++ SirmaOOPExam/Core/Interfaces/IRentalOrderService.cs
@@ -0,0 +1,11 @@
+﻿namespace SirmaOOPExam.Core.Interfaces
+{
+    internal interface IRentalOrderService
+    {
+        void AddRentalOrder(IRentalOrder rentalOrder);
+        List<IRentalOrder> GetAllRentalOrders();
+        List<ICar> GetCars();
+        List<ICustomer> GetCustomers();
+        IRentalOrder? GetRentalOrderById(int id);
+    }
+}
