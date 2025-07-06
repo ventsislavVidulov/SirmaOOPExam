20bcf058b57c7ea06168094fba8544b1d1f58bc3
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 18:48:31 +0300

    ReturnCarOption - init

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/ManageOrders/Options/ReturnCarOption.cs
@@ -0,0 +1,15 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.ManageOrders.Options
+{
+    internal class ReturnCarOption : IMenuOption
+    {
+        public string MenuText { get; } = "Return a car";
+        public int CommandNumber { get; } = 2;
+
+        public void Execute()
+        {
+            // Implementation for returning a car will go here
+        }
+    }
+}
