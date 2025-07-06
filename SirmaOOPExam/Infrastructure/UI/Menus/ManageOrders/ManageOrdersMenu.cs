6ef730a1281a727e73910adbf2107d3c776aed76
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 19:05:06 +0300

    ManageOrderMenu - add ExecuteOption() method

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/ManageOrders/ManageOrdersMenu.cs
@@ -0,0 +1,40 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+using SirmaOOPExam.Infrastructure.UserInterface;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.ManageOrders
+{
+    internal class ManageOrdersMenu : IMenu
+    {
+        public string Title { get; set; } = "Manage Orders";
+
+        public List<IMenuOption> Options { get; set; } = new();
+
+        public void RegisterOption(IMenuOption option)
+        {
+            Options.Add(option);
+        }
+        public int DisplayMenu()
+        {
+            ConsoleView.DisplayHeader(Title);
+            foreach (var menuItem in Options)
+            {
+                ConsoleView.DisplayOption(menuItem);
+            }
+            int choice = ConsoleView.GetIntegerInput($"Your choice(1-{Options.Count}): ");
+            return choice;
+        }
+
+        public void ExecuteOption(int optionNumber)
+        {
+            var option = Options.FirstOrDefault(o => o.CommandNumber == optionNumber);
+            if (option != null)
+            {
+                option.Execute();
+            }
+            else
+            {
+                ConsoleView.DisplayError("Invalid option selected.");
+            }
+        }
+    }
+}
