8cc0400d87cdef3be2e0ccbef154ef3a1a7a6714
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 21:37:33 +0300

    MenuOrchestrator - init

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/MenuOrchestartor.cs
@@ -0,0 +1,20 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus
+{
+    //not used
+    internal class MenuOrchestartor
+    {
+        private List<IMenu> _menus = new();
+
+        public void RegisterMenu(IMenu menu)
+        {
+            _menus.Add(menu);
+        }
+
+        public void DisplayMenu(string menuName)
+        {
+            _menus.FirstOrDefault(m => m.Title == menuName).DisplayMenu();
+        }
+    }
+}
