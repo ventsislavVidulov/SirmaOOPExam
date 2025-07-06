0697a1617be3c11a40f9e06e8a8400dfdec162a4
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 11:42:51 +0300

    IConverter - initialization

---
+++ SirmaOOPExam/Infrastructure/FileManagment/Interfaces/IConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace SirmaOOPExam.Infrastructure.FileManagment.Interfaces
+{
+    internal interface IConverter
+    {
+        T? ConvertToObject<T>(string[] valuesFromCSV) where T : class;
+        string[] ConvertToStringArray(object objectToBeConverted);
+    }
+}
