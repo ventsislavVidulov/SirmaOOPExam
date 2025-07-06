5be275b3acf221e097b01e00873f7f63ad045802
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 16:31:51 +0300

    ManageOrdersOption - initialization

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/MainMenu/Options/ManageOrdersOption.cs
@@ -0,0 +1,14 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.MainMenu.Options
+{
+    internal class ManageOrdersOption : IMenuOption
+    {
+        public string MenuText { get; } = "Manage Orders (rent a car, return a car)";
+        public int CommandNumber { get; } = 1;
+        public void Execute()
+        {
+            
+        }
+    }
+}
