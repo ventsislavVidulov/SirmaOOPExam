87f9fdaed44f8252bd73142b201845e875ff1d57
ventsislavVidulov <xcen3k@gmail.com>
2025-07-06 18:49:05 +0300

    BackToMainOption - init

---
+++ SirmaOOPExam/Infrastructure/UI/Menus/ManageOrders/Options/BackToMainOption.cs
@@ -0,0 +1,15 @@
+﻿using SirmaOOPExam.Infrastructure.UI.Menus.Interfaces;
+
+namespace SirmaOOPExam.Infrastructure.UI.Menus.ManageOrders.Options
+{
+    internal class BackToMainOption : IMenuOption
+    {
+        public string MenuText { get; } = "Back to Main Menu";
+        public int CommandNumber { get; } = 3;
+
+        public void Execute()
+        {
+            // Logic to return to the main menu will go here
+        }
+    }
+}
